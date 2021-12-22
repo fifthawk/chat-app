b347863cc30c24e251e4e9ac8f75cf77efeec0b4
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2021-12-21 20:12:47 -0600

    CHANGED URL AGAIN

--- client/src/context/SocketProvider.js
+++ client/src/context/SocketProvider.js
@@ -11,7 +11,7 @@ export function SocketProvider({ id, children }) {
   const [socket, setSocket] = useState();
 
   useEffect(() => {
-    const newSocket = io("http://192.168.86.23:4000", { query: { id } });
+    const newSocket = io("https://192.168.86.23:4000", { query: { id } });
     setSocket(newSocket);
 
     return () => newSocket.close();
